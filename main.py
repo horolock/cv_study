7d40b123054a8f65b6353d0c932e1b8b55b37526
horolock <horolock96@gmail.com>
2024-11-07 23:38:27 +0900

    study 11/07

--- main.py
+++ main.py
@@ -11,9 +11,30 @@ print(f"변수 타입: {type(image)}")
 
 cv2.imshow('frame', image)
 
+# 사이즈 변환
 image_small = cv2.resize(image, (100, 100))
-cv2.imshow('small_image',image_small)
+image_big = cv2.resize(image, dsize=None, fx=2, fy=2)
 
+# 대칭 변환
+image_flipped = cv2.flip(image, 0)  # 0 : 수평축 반전, 1 : 세로축 반전
+
+# 회전 변환
+height, width = image.shape
+matrix = cv2.getRotationMatrix2D((width / 2, height / 2), 30.0, 1.0)
+result = cv2.warpAffine(image, matrix, (width, height))
+
+# 자르기
+sliced_image = image[50:150, 50:150]
+
+# 원본이 변함
+# cropped_image = image[50:150, 50:150]
+# cropped_image[:] = 200
+
+# 깊은 복사 기능
+cropped_image = image[50:150, 50:150].copy()
+cropped_image[:] = 200
+
+cv2.imshow("frame", cropped_image)
 
 # End process
 cv2.waitKey(0)
