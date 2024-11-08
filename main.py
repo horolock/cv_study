eb73d3a1170d7d09e4fab0791b7989b5631a8bea
horolock <horolock96@gmail.com>
2024-11-09 00:38:46 +0900

    end first chapter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('like_lenna.png', cv2.IMREAD_GRAYSCALE)
 
@@ -34,7 +35,40 @@ sliced_image = image[50:150, 50:150]
 cropped_image = image[50:150, 50:150].copy()
 cropped_image[:] = 200
 
-cv2.imshow("frame", cropped_image)
+# cv2.imshow("frame", cropped_image)
+
+# Draw line
+space = np.zeros((500, 1000), dtype=np.uint8)
+line_color = 255
+space = cv2.line(space, (100, 100), (800, 400), line_color, 3, 1)
+
+# Draw circle
+circle_color = 255
+space = cv2.circle(space, (600, 200), 100, circle_color, 4, 1)
+
+# Draw rectangle
+space = cv2.rectangle(space, (500, 200), (800, 400), line_color, 5, 1)
+
+# Draw ellipse
+space = cv2.ellipse(space, (500, 300), (300, 200), 0, 90, 250, line_color, 4)
+
+# cv2.imshow('frame', space)
+
+# 다각형 그리기
+space2 = np.zeros((768, 1388), dtype=np.uint8)
+color = 255
+obj1 = np.array([
+    [300, 500], [500, 500], [400, 600], [200, 600]
+])
+
+obj2 = np.array([
+    [600, 500], [800, 500], [700, 200]
+])
+
+space2 = cv2.polylines(space2, [obj1], True, color, 3)
+space2 = cv2.fillPoly(space2, [obj2], color, 1)
+
+cv2.imshow('frame', space2)
 
 # End process
 cv2.waitKey(0)
