d5f02f6f04c071e71755b58865f0cd54a9f3f237
horolock <horolock96@gmail.com>
2024-11-09 16:29:13 +0900

    Start image processing basic

---
+++ image_process/main.py
@@ -0,0 +1,9 @@
+import keras
+
+### For downloading image
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context
+# url = "https://cobslab.com/wp-content/uploads/2022/02/ai-009-1.jpg"
+# image_path = keras.utils.get_file('C:/Users/hojoon/Developer/cv_study/image_process/content/image.jpg', origin=url)
+###
+
